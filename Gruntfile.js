64b79cb521657a4919940c0087808c1a179d5a2b
Danny Fritz <dannyfritz@gmail.com>
2013-08-26 20:24:29 -0500

    Updated grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,90 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    less:{
+    less: {
       development: {
         options: {
-          paths: ["less"],
+          paths: ['less'],
           report: 'min'
         },
         files: {
-          "dist/Gridles.css": "less/Gridles.less"
+          'dist/Gridles.css': 'build/Gridles.less'
         }
       },
       production: {
         options: {
-          paths: ["less"],
-          yuicompress: true,
-          report: 'min'
+          paths: ['less'],
+          report: 'min',
+          compress: true,
+          yuicompress: true
         },
         files: {
-          "dist/Gridles.css": "less/Gridles.less"
+          'build/Gridles.min.css': 'build/Gridles.less'
         }
       }
     },
-    uglify: {
+    asciify: { 
+      banner: {
+        text: 'Gridles',
+        options: {
+          font: 'alligator2',
+          log: true
+        }
+      }
+    },
+    concat: {
       options: {
         banner:
-          '/*! <%= pkg.name %>\n' +
-          '    Authors: <%= pkg.author %>\n' +
-          '    Built: <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          '/*!\n' + 
+          '<%= asciify_banner %>' +
+          '\n' +
+          '  <%= pkg.name %>\n' +
+          '  Authors: <%= pkg.author %>\n' +
+          '  Built: <%= grunt.template.today("yyyy-mm-dd") %>\n' +
           '*/\n'
       },
-      dist: {
-        src: 'dist/Gridles.css',
-        dest: 'dist/Gridles.min.js'
+      less: {
+        src: ['less/Gridles.less'],
+        dest: 'build/Gridles.less'
+      },
+      production: {
+        src: ['build/Gridles.min.css'],
+        dest: 'dist/Gridles.min.css'
       }
+    },
+    copy: {
+      less: {
+        src: 'build/Gridles.less',
+        dest: 'dist/Gridles.less',
+      }
+    },
+    clean: {
+      build: ['build/']
     },
     watch: {
       less: {
         files: ['less/*.less'],
-        tasks: ['less:development']
+        tasks: ['development']
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-asciify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['less:development', 'uglify:dist']);
-  grunt.registerTask('dev', ['less:development']);
-  grunt.registerTask('prod', ['less:production', 'uglify:dist']);
+  grunt.registerTask('default', function() {
+    grunt.task.run(['development']);
+    grunt.task.run(['production']);
+  });
+
+  grunt.registerTask('prod', function() {grunt.task.run(['production'])});
+  grunt.registerTask('production', ['asciify', 'concat:less', 'less:production', 'concat:production', 'clean:build']);
+
+  grunt.registerTask('dev', function() {grunt.task.run(['development'])});
+  grunt.registerTask('development',
+    ['asciify', 'concat:less', 'copy:less', 'less:development', 'clean:build']);
 };
